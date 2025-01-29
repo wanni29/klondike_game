1560cabd51a42f6e7ea2da7110b5d1a9d3e03b35
wanni30 <hubhy@nate.com>
2025-01-30 02:03:24 +0900

    카메라에 대한 개념, Sprite코드에 대한 개념 재적립 필요

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:klondike_game/klondike_game.dart';
 
 void main() {
-  final game = FlameGame();
+  // FlameGame 클래스란 ?
+  // 게임 상태의 중앙 저장소 역할 (게임루프 실행, 이벤트 전달)
+  final game = KlondikeGame();
   runApp(GameWidget(game: game));
 }
